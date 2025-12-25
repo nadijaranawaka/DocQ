fa85ab18f8d5cf362dd6eaa761d7492258b72fd7
nadijaranawaka <nadijaranawaka2727@gmail.com>
2025-12-25 19:45:10 +0530

    sample push to new branch

--- docq-main/src/App.jsx
+++ docq-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Document Q&A</h1>
+      <h1>Document_Q&A</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
