85ab82dabeb703ae26b834b0f04be3465eac62e1
mrphreacker <najahmohomed123@gmail.com>
2026-06-01 15:58:28 +0530

    model ini

--- docq-ai/app/config/settings.py
+++ docq-ai/app/config/settings.py
@@ -2,3 +2,5 @@
 EMBEDDING_MODEL = "all-MiniLM-L6-v2"
 CHUNK_SIZE = 800
 CHUNK_OVERLAP = 100
+MODEL_GEM = "gemini-2.5-flash"
+TEMP = 0.7
---
+++ docq-ai/model/bottest.py
@@ -0,0 +1,49 @@
+import os
+import sys
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+load_dotenv()
+# ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+# sys.path.append(ROOT_DIR)
+from app.config.settings import MODEL_GEM, TEMP
+
+
+def chatbot():
+
+    API_KEY = os.getenv("GEM_API_KEY")
+    if not API_KEY:
+        print("Please set MY_API_KEY environment variable")
+        sys.exit(1)
+
+    print("chatbot working")
+
+    client=genai.Client(api_key=API_KEY)
+
+    system = (
+        "be good, be smart"
+        "dont ignore rules"
+    )
+
+    chat = client.chats.create(
+        model =   MODEL_GEM,
+        config = types.GenerateContentConfig(system_instruction= system,temperature = TEMP)
+    )
+
+    while True:
+        try:
+            user_in = input("You: ")
+
+            if user_in.lower().strip() == "quit":
+                break
+
+            if user_in.strip():
+                continue
+
+            responses = chat.send_message(user_in)
+            print(responses)
+
+        except Exception as e:
+            print(e)
+
+chatbot()
