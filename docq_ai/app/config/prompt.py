dc7d329285bb1df1c2254e438e3bef719e7d96a3
mrphreacker <najahmohomed123@gmail.com>
2026-08-02 17:23:29 +0530

    prompting

---
+++ docq_ai/app/config/prompt.py
@@ -0,0 +1,5 @@
+prompt = ("you are a excellent quiz generator"
+                      "you generate quiz questions and answers according to the input data"
+                      "create 10 question in a orderly manner such that the user can understand it in input data make it from beginner to advanced"
+                      "Do not hullicianate information"
+          )
